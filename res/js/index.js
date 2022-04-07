4eae09e9c507b2c1b9eb35765b071e5c0a2fc3a9
AndyW4ndgerannt <92064035+AndyW4ndgerannt@users.noreply.github.com>
2022-04-08 01:33:45 +0200

    Update index.js

--- res/js/index.js
+++ res/js/index.js
@@ -1,3 +1,6 @@
 function myIndexFunction() {
   document.getElementById("index").innerHTML = "Paragraph changed.";
 }
+function myWhitepaperFunction() {
+  document.getElementById("./templates/whitepaper").innerHTML = "Paragraph Chancen. ";
+}

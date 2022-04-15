5d0eef6f1ec671e863bd798d2a90ce4aeb9fc9bd
AndyW4ndgerannt <92064035+AndyW4ndgerannt@users.noreply.github.com>
2022-04-15 17:48:22 +0200

    Update index.js

--- res/js/index.js
+++ res/js/index.js
@@ -1,6 +1,40 @@
-function myIndexFunction() {
-  document.getElementById("index").innerHTML = "Paragraph changed.";
-}
-function myWhitepaperFunction() {
-  document.getElementById("./templates/whitepaper").innerHTML = "Paragraph Chancen. ";
+{
+  "Titel" : "Titel",
+  "homeTown" : "Lebach",
+  "since" : 2022,
+  "active" : true,
+  "members" : [
+    {
+      "name" : "Andy Ge",
+      "age" : 29,
+      "secretIdentity" : "Dan Jukes",
+      "powers" : [
+        "Radiation resistance",
+        "Turning tiny",
+        "Radiation blast"
+      ]
+    },
+    {
+      "name" : "Tim Tommersen",
+      "age" : 39,
+      "position" : "Jane Wilson",
+      "powers" : [
+        "Million tonne punch",
+        "Damage resistance",
+        "Superhuman reflexes"
+      ]
+    },
+    {
+      "name" : "Captain Homo",
+      "age" : 1000000,
+      "secretIdentity" : "Unknown",
+      "powers" : [
+        "Immortality",
+        "Heat Immunity",
+        "Inferno",
+        "Teleportation",
+        "Interdimensional travel"
+      ]
+    }
+  ]
 }
